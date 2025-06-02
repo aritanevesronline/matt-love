a7485f1f29a14664b4aad072725aa92d2727fb9c
aritanevesronline <kitinha212@gmail.com>
2025-06-02 17:41:51 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -72,7 +72,7 @@ var x = setInterval(function () {
     var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
     var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-    document.getElementById('countdown').innerHTML = days + "d" + hours + "h " + minutes + "m " + seconds + "s ";
+    document.getElementById('countdown').innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
 
     if (distance < 0 & togetherTime > 0) {
         clearInterval(x);
